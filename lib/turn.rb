effacf2fe78ce9bbe8cd1780f2dec674f2eb5c2b
Anthony Hillen <ah205@students.uwf.edu>
2019-01-07 22:02:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-def valid_turn?(board,index)
+def valid_move?(board,index)
   index.between?(0,8) && !(position_taken?(board,index))
 end
 
@@ -7,7 +7,7 @@ def position_taken?(board,index)
 end
 
 def display_board(board)
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts " #{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
   puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-----------"
